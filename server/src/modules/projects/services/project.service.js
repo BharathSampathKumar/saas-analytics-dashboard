14c0965307d1ba2a87cfb1693a9aa8221d06e43f
Bharath Sampath Kumar <39097638+BharathSampathKumar@users.noreply.github.com>
2026-04-21 15:17:25 +0530

    feat(projects): enforce workspace ownership validation

--- server/src/modules/projects/services/project.service.js
+++ server/src/modules/projects/services/project.service.js
@@ -1,18 +1,20 @@
-const Project = require("../../../models/Project");
-const generateApiKey = require("../../../utils/generateApiKey");
+const Workspace = require("../../../models/Workspace");
 
-exports.createProject = async ({ name, workspaceId }) => {
+exports.createProject = async ({ name, workspaceId, userId }) => {
+  const workspace = await Workspace.findOne({
+    _id: workspaceId,
+    "members.userId": userId,
+  });
+
+  if (!workspace) {
+    throw new Error("Unauthorized workspace access");
+  }
+
   const apiKey = generateApiKey();
 
-  const project = await Project.create({
+  return Project.create({
     name,
     workspaceId,
     apiKey,
   });
-
-  return project;
-};
-
-exports.getProjectsByWorkspace = async (workspaceId) => {
-  return Project.find({ workspaceId }).sort({ createdAt: -1 });
 };
