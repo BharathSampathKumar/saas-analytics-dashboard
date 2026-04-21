3ea465c0be049a12a6d4e7b0c30d2bca5e6bfd2b
Bharath Sampath Kumar <39097638+BharathSampathKumar@users.noreply.github.com>
2026-04-21 15:28:32 +0530

    test: add database test setup utilities

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>/server/tests"],
+  testMatch: ["**/tests/**/*.test.js"],
   setupFilesAfterEnv: ["<rootDir>/server/tests/setup.js"],
 };
