16da60fb5f19fe79c4c0ac1d7c9e485d7eafce47
Bharath Sampath Kumar <39097638+BharathSampathKumar@users.noreply.github.com>
2026-04-21 14:21:06 +0530

    feat(projects): implement project service with create and fetch

---
+++ server/src/modules/projects/services/project.service.js
@@ -0,0 +1,18 @@
+const Project = require("../../../models/Project");
+const generateApiKey = require("../../../utils/generateApiKey");
+
+exports.createProject = async ({ name, workspaceId }) => {
+  const apiKey = generateApiKey();
+
+  const project = await Project.create({
+    name,
+    workspaceId,
+    apiKey,
+  });
+
+  return project;
+};
+
+exports.getProjectsByWorkspace = async (workspaceId) => {
+  return Project.find({ workspaceId }).sort({ createdAt: -1 });
+};
